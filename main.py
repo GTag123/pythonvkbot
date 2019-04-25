9f5b6ac94f609e81b2c6b663d747d3f7dfe427eb
Tagir <gtagir11@yandex.ru>
2019-04-25 23:25:08 +0500

    'nothing)'

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/bot')
+@app.route('/bot', methods=['POST'])
 def hello_world():
-    return 'Hello World!'
+    return request.json
 if __name__ == '__main__':
     app.run()
