0ddf9ba03fb7789a8e4d84848049cb64a0f40eb8
Tagir <gtagir11@yandex.ru>
2019-04-26 14:32:22 +0500

    'nothing)'

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+	print('дарова')
     return '<h1>Ku</h1>'
 
-@app.route('/bot', methods=['POST'])
-def main():
-	content = request.get_json()
-    return content
+# @app.route('/bot', methods=['POST'])
+# def main():
+# 	content = request.get_json()
+#     return content
 
 if __name__ == '__main__':
     app.run()
