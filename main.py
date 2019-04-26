95e432c93c8fe067b510feef766fafbcbef2b9e9
Tagir <gtagir11@yandex.ru>
2019-04-26 15:48:38 +0500

    'nothing)'

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask, request
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+	print(os.environ)
 	return '<h1>Ku</h1>'
 
-@app.route('/bot', methods=['GET','POST'])
+@app.route('/bot', methods=['POST'])
 def main():
-	print('test\n')
 	content = str(request.get_json(force=True))
+	print('Json: ', content)
 	return content
 
 if __name__ == '__main__':
