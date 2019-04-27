15a0d08b064adcacc55bfe6d14a2dfa42f6ebf0d
Tagir <gtagir11@yandex.ru>
2019-04-27 13:55:58 +0500

    sql start

--- main.py
+++ main.py
@@ -10,17 +10,11 @@ secret = getenv('secret')
 confirm = getenv('confirmation')
 DATABASE_URL = getenv('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-sql = "SELECT * FROM messages"
-
-try:
-    cur = conn.cursor()
-    # cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) # by column name
-    cur.execute(sql)
-    data = cur.fetchall()
-except psycopg2.Error as err:
-    print("Query error: {}".format(err))
-print('Data: '+ str(data))
+with conn:
+	with conn.cursor(cursor_factory=DictCursor) as cursor:
+		sql = "SELECT * FROM messages"
+		cursor.execute(sql)
+		print('Data: ' + str(cursor.fetchall))
 
 @app.route('/')
 def hello_world():
