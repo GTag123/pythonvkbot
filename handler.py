76f069e8bb15347b29613c640d8e03d188d568dd
Tagir <gtagir11@yandex.ru>
2019-04-29 21:22:46 +0500

    hello

--- handler.py
+++ handler.py
@@ -12,25 +12,24 @@ db = database.Database(DATABASE_URL)
 
 def main(content):
 	if content['secret'] != secret:
-		return 'Вы дурак, пошёл в жопу!'
-
+		return 'Secret key error! Please, go away!'
 # ---------------------Main handler-----------------------
 	elif content['type'] == 'message_new':
 		vk_id = content['object']['from_id']
-		message = content['object']['text']
+		message = content['object']['text'].split(maxsplit=1)
 		getname = requests.post('https://api.vk.com/method/users.get', data={
     		'user_ids': vk_id,
     		'access_token': token,
     		'v': '5.95'}).json()['response'][0]
-
-		db.new_action("INSERT INTO messages(vk_id, name, message) VALUES('{}', '{} {}', '{}');".format(vk_id, getname['first_name'], getname['last_name'], message))
 		sending_params = {
 			'peer_id': vk_id,
-			'message': 'Ваше сообщение: %s\n%s' % (message, 'Последняя запись: ' + str(db.select('messages')[-1])),
+			'message': 'Мои команды:\n!привет - бот скажет тебе привет',
 			'access_token': token,
 			'v': '5.95',
 			'random_id': randint(0, 99999)
 			}
+		if message[0].lower() == '!привет':
+			sending_params['message'] = f'[id{vk_id}|{getname['first_name']}], привет!'
 		requests.post('https://api.vk.com/method/messages.send', data=sending_params) # sending message
 # --------------------------------------------------------
 	elif content['type'] == 'confirmation':
