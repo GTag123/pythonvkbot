40632c95a9ee6fe09315afb7ed5e72e3989db01c
Tagir <gtagir11@yandex.ru>
2019-04-29 22:23:05 +0500

    hello

--- handler.py
+++ handler.py
@@ -17,10 +17,7 @@ def main(content):
 	elif content['type'] == 'message_new':
 		vk_id = content['object']['from_id']
 		message = content['object']['text'].split(maxsplit=1)
-		getname = requests.post('https://api.vk.com/method/users.get', data={
-    		'user_ids': vk_id,
-    		'access_token': token,
-    		'v': '5.95'}).json()['response'][0]
+		message[0] = message[0]..lower()
 		sending_params = {
 			'peer_id': vk_id,
 			'message': 'Мои команды:\n!привет - бот скажет тебе привет',
@@ -28,8 +25,14 @@ def main(content):
 			'v': '5.95',
 			'random_id': randint(0, 99999)
 			}
-		if message[0].lower() == '!привет':
+		if message[0] == '!привет':
+			getname = requests.post('https://api.vk.com/method/users.get', data={
+    		'user_ids': vk_id,
+    		'access_token': token,
+    		'v': '5.95'}).json()['response'][0]
 			sending_params['message'] = f"[id{vk_id}|{getname['first_name']}], привет!"
+		elif message[0] = '!анекдот':
+			sending_params['message'] = requests.post('http://rzhunemogu.ru/RandJSON.aspx?CType=1').json()['content']
 		requests.post('https://api.vk.com/method/messages.send', data=sending_params) # sending message
 # --------------------------------------------------------
 	elif content['type'] == 'confirmation':
