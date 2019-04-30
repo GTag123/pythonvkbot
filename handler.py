e04842d3bb3f9ffa7df310df79de6d4032005f62
Tagir <gtagir11@yandex.ru>
2019-04-30 22:38:58 +0500

    nickname

--- handler.py
+++ handler.py
@@ -27,7 +27,7 @@ def main(content):
 				'user_ids': vk_id,
 				'access_token': token,
 				'v': '5.95'}).json()['response'][0]
-			db.new_action(f"INSERT INTO users (vk_id, name, balance, reg_time) VALUES ({vk_id}, {vkname['first_name']}, '1000', to_timestamp({content['object']['date']}));")
+			db.new_action(f"INSERT INTO users (vk_id, name, balance, reg_time) VALUES ({vk_id}, '{vkname['first_name']}', 1000, to_timestamp({content['object']['date']}));")
 		nickname = db.select(f"SELECT name FROM users WHERE vk_id = {vk_id};")[0]['name']
 
 		sending_params = {
