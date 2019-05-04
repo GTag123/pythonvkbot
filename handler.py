d2f1e6f0fe5f444cabb9f6c4cfa03b839dfd99c2
Tagir <gtagir11@yandex.ru>
2019-05-04 10:43:59 +0500

    sell

--- handler.py
+++ handler.py
@@ -85,6 +85,7 @@ def sell(id, message):
 		db.new_action(f"UPDATE users SET balance = balance + ({thinginfo['price'] * 0.6}); UPDATE own SET {type[0]} = DEFAULT WHERE id = {id};""")
 		return f"вы успешно продали {thinginfo['name']} за {thinginfo['price'] * 0.6} монет (60% стоимости)\nДля покупки нового имущества - !магазин"
 	except KeyError:
+		print(message)
 		return "1"
 
 def shoplist():
